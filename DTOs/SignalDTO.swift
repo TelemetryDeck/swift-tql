3a20e94a8edcd7e114672a3150aa80b65737dcfa
Daniel Jilg <winsmith@winsmith.de>
2021-05-12 18:09:51 +0200

    SignalDruidStructure

--- DTOs/SignalDTO.swift
+++ DTOs/SignalDTO.swift
@@ -18,9 +18,36 @@ public extension DTO {
         public var type: String
         public var payload: [String: String]?
     }
+
+    struct SignalDruidStructure: Codable, Hashable {
+        public var id: UUID?
+        public var appID: UUID?
+        public var count: Int?
+        public var receivedAt: Date
+        public var clientUser: String
+        public var sessionID: String?
+        public var type: String
+        public var payload: String
+
+        func toSignal() -> Signal {
+            let payloadJSON = payload.replacingOccurrences(of: "\\", with: "").data(using: .utf8)
+            var actualPayload = [String: String]()
+            if let payloadJSON = payloadJSON,
+               let payloadArray = try? JSONDecoder().decode([String].self, from: payloadJSON) {
+                for entry in payloadArray {
+                    let subsequence = entry.split(separator: ":")
+                    if let key = subsequence.first, let value = subsequence.last {
+                        actualPayload[String(key)] = String(value)
+                    }
+                }
+            }
+            return Signal(id: id, appID: appID, count: count, receivedAt: receivedAt, clientUser: clientUser, sessionID: sessionID, type: type, payload: [:])
+        }
+    }
 }
 
 #if canImport(Vapor)
     import Vapor
     extension DTO.Signal: Content {}
+    extension DTO.SignalDruidStructure: Content {}
 #endif
