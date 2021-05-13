3e5e456d65be79bbce3d60fa0d254948bf42d0f8
Daniel Jilg <winsmith@winsmith.de>
2021-05-13 13:49:50 +0200

    Add LexiconSignalDTO

---
+++ DTOs/LexiconSignalDTO.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//
+//
+//  Created by Daniel Jilg on 12.05.21.
+//
+
+import Foundation
+
+public extension DTO {
+    struct LexiconSignalDTO: Codable, Hashable, Identifiable {
+        public var id: String { return type }
+        public var type: String
+        public var signalCount: Int
+        public var userCount: Int
+    }
+}
+
+#if canImport(Vapor)
+    import Vapor
+    extension DTO.LexiconSignalDTO: Content {}
+#endif
