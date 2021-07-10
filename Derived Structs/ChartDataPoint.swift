17515f0ad471eb414ae860d4e174fb4019ca086a
Daniel Jilg <winsmith@winsmith.de>
2021-07-10 16:17:14 +0200

    Update ChartDataPoint for better memory useage
    ChartDataPoint now does not instantiate a new numberFormatter each time it needs one, and pre-calculates Date and Double values, hopefully saving some memory.

--- Derived Structs/ChartDataPoint.swift
+++ Derived Structs/ChartDataPoint.swift
@@ -5,38 +5,29 @@ public struct ChartDataPoint: Hashable, Identifiable {
     public var id: String { xAxisValue }
 
     public let xAxisValue: String
+    public let xAxisDate: Date?
+    
     public let yAxisValue: String?
+    public let yAxisDouble: Double?
     
     public init(xAxisValue: String, yAxisValue: String?) {
         self.xAxisValue = xAxisValue
         self.yAxisValue = yAxisValue
+        
+        if let yAxisValue = yAxisValue {
+            yAxisDouble = Double(yAxisValue)
+        } else {
+            yAxisDouble = nil
+        }
+        
+        if #available(macOS 10.14, iOS 14.0, *) {
+            xAxisDate = Formatter.iso8601noFS.date(from: xAxisValue) ?? Formatter.iso8601.date(from: xAxisValue)
+        } else {
+            xAxisDate = nil
+        }
     }
     
     init(insightData: DTO.InsightData) {
         self.init(xAxisValue: insightData.xAxisValue, yAxisValue: insightData.yAxisValue)
-    }
-    
-    private let numberFormatter: NumberFormatter = {
-        let numberFormatter = NumberFormatter()
-        numberFormatter.numberStyle = .decimal
-        numberFormatter.usesGroupingSeparator = true
-        return numberFormatter
-    }()
-
-    public var yAxisNumber: NSNumber? {
-        guard let yAxisValue = yAxisValue else { return NSNumber(value: 0) }
-        return numberFormatter.number(from: yAxisValue)
-    }
-
-    public var yAxisDouble: Double? {
-        yAxisNumber?.doubleValue
-    }
-
-    public var xAxisDate: Date? {
-        if #available(macOS 10.14, iOS 14.0, *) {
-            return Formatter.iso8601noFS.date(from: xAxisValue) ?? Formatter.iso8601.date(from: xAxisValue)
-        } else {
-            return nil
-        }
     }
 }
