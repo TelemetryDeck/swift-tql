a8ac5c527567fa3b8018c6a78ee6b83392a60398
Daniel Jilg <winsmith@winsmith.de>
2021-07-26 10:14:41 +0200

    Add isCurrentPeriod function to ChartDataSet

--- Derived Structs/ChartDataSet.swift
+++ Derived Structs/ChartDataSet.swift
@@ -24,4 +24,23 @@ public struct ChartDataSet {
         highestValue = self.data.reduce(0) { max($0, $1.yAxisDouble ?? 0) }
         lowestValue = 0
     }
+    
+    /// `true` if the point represents the current day/week/month/etc, and therefore contains
+    /// incomplete data.
+    public func isCurrentPeriod(_ chartDataPoint: ChartDataPoint) -> Bool {
+        let groupByPeriod = groupBy ?? .day
+
+        guard let date = chartDataPoint.xAxisDate else { return false }
+
+        switch groupByPeriod {
+        case .hour:
+            return date.isInCurrent(.hour)
+        case .day:
+            return date.isInToday
+        case .week:
+            return date.isInCurrentWeek
+        case .month:
+            return date.isInCurrentMonth
+        }
+    }
 }
