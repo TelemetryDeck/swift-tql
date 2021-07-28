1409ed0d3f484be97136651a2faf20e20159e8d8
fireantprincess <carly.weird@gmail.com>
2021-07-28 19:00:16 +0200

    add tier to AppAdminEntry

--- DTOs/AppAdminEntry.swift
+++ DTOs/AppAdminEntry.swift
@@ -8,6 +8,34 @@ public extension DTO {
         public let organisationID: UUID?
         public let signalCount: Int
         public let userCount: Int
+        public var tier: TierType {
+            if signalCount > 5_000_000 {
+                return .tier2
+            }
+            if signalCount > 100_000 {
+                return .tier1
+            }
+            if signalCount > 100 {
+                return .free
+            }
+            return .explorers
+        }
+                
+        public enum TierType {
+            case explorers
+            case free
+            case tier1
+            case tier2
+            
+            var stringDescription: String {
+                switch self {
+                    case .explorers: return "Explorers"
+                    case .free: return "Free Tier"
+                    case .tier1: return "Tier 1"
+                    case .tier2: return "Tier 2"
+                }
+            }
+        }
     }
 }
 
