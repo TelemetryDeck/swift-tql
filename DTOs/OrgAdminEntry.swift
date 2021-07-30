1508292fda95bcd1621d95a436e9184eae447c40
fireantprincess <carly.weird@gmail.com>
2021-07-30 18:54:19 +0200

    what

--- DTOs/OrgAdminEntry.swift
+++ DTOs/OrgAdminEntry.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftUI
+
+public extension DTO {
+    struct OrgAdminEntry: Codable, Identifiable {
+        public let id: UUID
+        public let organisationName: String?
+        public var appAdminEntries: [DTO.AppAdminEntry]
+        public var signalCount: Int = 0
+    }
+}
+
+#if canImport(Vapor)
+import Vapor
+
+extension DTO.OrgAdminEntry: Content {}
+#endif
+
