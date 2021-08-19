4b42534bbd6b6b629c60b5d11e51a8354e0efa8c
fireantprincess <carly.weird@gmail.com>
2021-08-19 23:07:23 +0200

    remove tier stuff
    no longer needed in appadminentry since I put it in appadminservice

--- DTOs/AppAdminEntry.swift
+++ DTOs/AppAdminEntry.swift
@@ -9,46 +9,6 @@ public extension DTO {
         public let organisationID: UUID?
         public let signalCount: Int
         public let userCount: Int
-        public var tier: TierType {
-            if signalCount > 5_000_000 {
-                return .tier2
-            }
-            if signalCount > 100_000 {
-                return .tier1
-            }
-            if signalCount > 100 {
-                return .free
-            }
-            return .explorers
-        }
-                
-        public enum TierType {
-            case explorers
-            case free
-            case tier1
-            case tier2
-            
-            #if canImport(Vapor)
-            #else
-            var stringDescription: String {
-                switch self {
-                    case .explorers: return "Explorers"
-                    case .free: return "Free Tier"
-                    case .tier1: return "Tier 1"
-                    case .tier2: return "Tier 2"
-                }
-            }
-
-            var tierColor: Color {
-                switch self {
-                case .explorers: return Color.secondary.opacity(0.9)
-                case .free: return Color.mint.opacity(0.7)
-                case .tier1: return Color.indigo.opacity(0.9)
-                case .tier2: return Color.telemetryOrange.opacity(0.7)
-                }
-            }
-            #endif
-        }
     }
 }
 
