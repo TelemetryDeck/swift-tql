8f98015de4e51202b88019ea6689494d6dbfad77
Daniel Jilg <winsmith@winsmith.de>
2021-10-08 21:30:37 +0200

    Use DruidContext instead of outdated DruidSQLQueryContext

--- DruidDTOs/DruidSQLQuery.swift
+++ DruidDTOs/DruidSQLQuery.swift
@@ -1,16 +1,11 @@
 import Foundation
 
-struct DruidSQLQueryContext: Codable {
-    let skipEmptyBuckets: Bool
-}
-
 struct DruidSQLQuery: Codable {
     let query: String
-    let context: DruidSQLQueryContext
+    let context: DruidContext
 }
 
 #if canImport(Vapor)
 import Vapor
 extension DruidSQLQuery: Content {}
-extension DruidSQLQueryContext: Content {}
 #endif
