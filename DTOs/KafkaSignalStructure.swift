d11a3c6ac26c818f2a2379ddc495e76fbc5bbb67
Daniel Jilg <winsmith@winsmith.de>
2021-10-21 13:23:36 +0200

    Add denormalized payload items to KafkaSignalStructure

--- DTOs/KafkaSignalStructure.swift
+++ DTOs/KafkaSignalStructure.swift
@@ -17,6 +17,19 @@ struct KafkaSignalStructureWithDict: Codable {
     let type: String
     let payload: [String: String]
 
+    var platform: String? { payload["platform"] }
+    var systemVersion: String? { payload["systemVersion"] }
+    var majorSystemVersion: String? { payload["majorSystemVersion"] }
+    var majorMinorSystemVersion: String? { payload["majorMinorSystemVersion"] }
+    var appVersion: String? { payload["appVersion"] }
+    var buildNumber: String? { payload["buildNumber"] }
+    var modelName: String? { payload["modelName"] }
+    var architecture: String? { payload["architecture"] }
+    var operatingSystem: String? { payload["operatingSystem"] }
+    var targetEnvironment: String? { payload["targetEnvironment"] }
+    var locale: String? { payload["locale"] }
+    var telemetryClientVersion: String? { payload["telemetryClientVersion"] }
+
     func toTagStructure() -> KafkaSignalStructureWithTags {
         KafkaSignalStructureWithTags(
             receivedAt: receivedAt,
@@ -25,7 +38,23 @@ struct KafkaSignalStructureWithDict: Codable {
             clientUser: clientUser,
             sessionID: sessionID,
             type: type,
-            payload: KafkaSignalStructureWithDict.convertToMultivalueDimension(payload: payload)
+            payload: KafkaSignalStructureWithDict.convertToMultivalueDimension(payload: payload),
+            
+            // Pull common payload keys out of the payload dictionary and put them one level up. This can help
+            // increase performance on the druid level by treating these as fields instead of having to
+            // string-search through payload for them.
+            platform: platform,
+            systemVersion: systemVersion,
+            majorSystemVersion: majorSystemVersion,
+            majorMinorSystemVersion: majorMinorSystemVersion,
+            appVersion: appVersion,
+            buildNumber: buildNumber,
+            modelName: modelName,
+            architecture: architecture,
+            operatingSystem: operatingSystem,
+            targetEnvironment: targetEnvironment,
+            locale: locale,
+            telemetryClientVersion: telemetryClientVersion
         )
     }
     
@@ -51,4 +80,18 @@ struct KafkaSignalStructureWithTags: Codable {
     let sessionID: String
     let type: String
     let payload: [String]
+
+    // Denormalized Payload Items
+    let platform: String?
+    let systemVersion: String?
+    let majorSystemVersion: String?
+    let majorMinorSystemVersion: String?
+    let appVersion: String?
+    let buildNumber: String?
+    let modelName: String?
+    let architecture: String?
+    let operatingSystem: String?
+    let targetEnvironment: String?
+    let locale: String?
+    let telemetryClientVersion: String?
 }
