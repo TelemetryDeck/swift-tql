3421b0dd0821615a88e2d0593c7572e1256eba9e
Daniel Jilg <winsmith@winsmith.de>
2021-12-01 21:46:15 +0100

    Add CountryCode to RegistrationRequestBody

--- Sources/DataTransferObjects/DTOs/RegistrationRequestBody.swift
+++ Sources/DataTransferObjects/DTOs/RegistrationRequestBody.swift
@@ -18,6 +18,7 @@ public extension DTOv1 {
         public var userPassword: String = ""
         public var userPasswordConfirm: String = ""
         public var receiveMarketingEmails: Bool = false
+        public var countryCode: String? = ""
 
         public var isValid: ValidationState {
             if organisationName.isEmpty || userFirstName.isEmpty || userEmail.isEmpty || userPassword.isEmpty {
