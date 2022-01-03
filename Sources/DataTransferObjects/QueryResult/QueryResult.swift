248bebe60a3683719c6bbe30374302b4c2de75a3
Daniel Jilg <winsmith@winsmith.de>
2022-01-03 22:20:32 +0100

    Add TopNResults Types

--- Sources/DataTransferObjects/QueryResult/QueryResult.swift
+++ Sources/DataTransferObjects/QueryResult/QueryResult.swift
@@ -2,6 +2,7 @@ import Foundation
 
 public enum QueryResult: Codable, Hashable, Equatable {
     case timeSeries(TimeSeriesQueryResult)
+    case topN(TopNQueryResult)
     case groupBy(GroupByQueryResult)
 
     enum CodingKeys: String, CodingKey {
@@ -15,6 +16,8 @@ public enum QueryResult: Codable, Hashable, Equatable {
         switch type {
         case "timeSeriesResult":
             self = .timeSeries(try TimeSeriesQueryResult(from: decoder))
+        case "topNResult":
+            self = .topN(try TopNQueryResult(from: decoder))
         case "groupByResult":
             self = .groupBy(try GroupByQueryResult(from: decoder))
         default:
@@ -29,6 +32,9 @@ public enum QueryResult: Codable, Hashable, Equatable {
         case .timeSeries(let timeSeries):
             try container.encode("timeSeriesResult", forKey: .type)
             try timeSeries.encode(to: encoder)
+        case .topN(let topN):
+            try container.encode("topNResult", forKey: .type)
+            try topN.encode(to: encoder)
         case .groupBy(let columnComparison):
             try container.encode("groupByResult", forKey: .type)
             try columnComparison.encode(to: encoder)
@@ -40,7 +46,7 @@ public struct TimeSeriesQueryResult: Codable, Hashable, Equatable {
     public init(rows: [TimeSeriesQueryResultRow]) {
         self.rows = rows
     }
-    
+
     public let rows: [TimeSeriesQueryResultRow]
 }
 
@@ -66,6 +72,93 @@ public struct GroupByQueryResult: Codable, Hashable, Equatable {
 
     public let timestamp: Date
     public let result: [String: String]
+}
+
+/// TopN queries return a sorted set of results for the values in a given dimension according to some criteria.
+///
+/// Conceptually, they can be thought of as an approximate GroupByQuery over a single dimension with an Ordering spec.
+/// TopNs are much faster and resource efficient than GroupBys for this use case. These types of queries take a topN query
+///  object and return an array of JSON objects where each object represents a value asked for by the topN query.
+public struct TopNQueryResult: Codable, Hashable, Equatable {
+    public init(rows: [TopNQueryResultRow]) {
+        self.rows = rows
+    }
+
+    public let rows: [TopNQueryResultRow]
+}
+
+public struct TopNQueryResultRow: Codable, Hashable, Equatable {
+    public init(timestamp: Date, result: [TopNQueryResultRowItem]) {
+        self.timestamp = timestamp
+        self.result = result
+    }
+
+    public let timestamp: Date
+    public let result: [TopNQueryResultRowItem]
+}
+
+public struct TopNQueryResultRowItem: Codable, Hashable, Equatable {
+    public init(metrics: [String: Double], dimensions: [String: String], nullValues: [String] = []) {
+        self.metrics = metrics
+        self.dimensions = dimensions
+        self.nullValues = nullValues
+    }
+
+    public let metrics: [String: Double]
+    public let dimensions: [String: String]
+    public let nullValues: [String]
+
+    public init(from decoder: Decoder) throws {
+        var metrics = [String: Double]()
+        var dimensions = [String: String]()
+        var nullValues = [String]()
+
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        for key in container.allKeys {
+            if let stringElement = try? container.decode(String.self, forKey: key) {
+                dimensions[key.stringValue] = stringElement
+            }
+
+            else if let doubleElement = try? container.decode(Double.self, forKey: key) {
+                metrics[key.stringValue] = doubleElement
+            }
+
+            else {
+                nullValues.append(key.stringValue)
+            }
+        }
+
+        self.metrics = metrics
+        self.dimensions = dimensions
+        self.nullValues = nullValues
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        for key in metrics.keys {
+            try container.encode(metrics[key], forKey: .init(stringValue: String(key))!)
+        }
+
+        for key in dimensions.keys {
+            try container.encode(dimensions[key], forKey: .init(stringValue: String(key))!)
+        }
+
+        for key in nullValues {
+            try container.encodeNil(forKey: .init(stringValue: key)!)
+        }
+    }
+
+    struct CodingKeys: CodingKey {
+        var stringValue: String
+        init?(stringValue: String) {
+            self.stringValue = stringValue
+        }
+
+        var intValue: Int?
+        init?(intValue: Int) {
+            return nil
+        }
+    }
 }
 
 // MARK: - Legacy Structs
---
+++ Tests/DataTransferObjectsTests/TopNResultTests.swift
@@ -0,0 +1,168 @@
+//
+//  TopNResultTests.swift
+//
+//
+//  Created by Daniel Jilg on 03.01.22.
+//
+
+import DataTransferObjects
+import XCTest
+
+class TopNResultTests: XCTestCase {
+    static let firstMonthDate: Date = Formatter.iso8601.date(from: "2021-12-01T00:00:00.000Z")!
+    static let secondMonthDate: Date = Formatter.iso8601.date(from: "2022-01-01T00:00:00.000Z")!
+    
+    let emptyResult = """
+    [
+      {
+        "result": [],
+        "timestamp": "2021-12-01T00:00:00+0000"
+      },
+      {
+        "result": [],
+        "timestamp": "2022-01-01T00:00:00+0000"
+      }
+    ]
+    """
+    .filter { !$0.isWhitespace }
+    
+    let decodedEmptyExampleResult = [
+        TopNQueryResultRow(timestamp: firstMonthDate, result: []),
+        TopNQueryResultRow(timestamp: secondMonthDate, result: []),
+    ]
+    
+    let exampleResult = """
+    [
+      {
+        "result": [
+          {
+            "appVersion": "276",
+            "count": 1
+          },
+          {
+            "appVersion": "324",
+            "count": 1
+          },
+          {
+            "appVersion": "408",
+            "count": 35
+          }
+        ],
+        "timestamp": "2021-12-01T00:00:00+0000"
+      },
+      {
+        "result": [
+          {
+            "appVersion": null,
+            "count": 2
+          },
+          {
+            "appVersion": "411",
+            "count": 6
+          },
+          {
+            "appVersion": "448",
+            "count": 9
+          }
+        ],
+        "timestamp": "2022-01-01T00:00:00+0000"
+      }
+    ]
+    """
+    .filter { !$0.isWhitespace }
+    
+    let decodedExampleResult = [
+        TopNQueryResultRow(timestamp: firstMonthDate, result: [
+            .init(metrics: ["count": 1], dimensions: ["appVersion": "276"]),
+            .init(metrics: ["count": 1], dimensions: ["appVersion": "324"]),
+            .init(metrics: ["count": 35], dimensions: ["appVersion": "408"]),
+        ]),
+        TopNQueryResultRow(timestamp: secondMonthDate, result: [
+            .init(metrics: ["count": 2], dimensions: [:], nullValues: ["appVersion"]),
+            .init(metrics: ["count": 6], dimensions: ["appVersion": "411"]),
+            .init(metrics: ["count": 9], dimensions: ["appVersion": "448"]),
+        ]),
+    ]
+    
+    let resultRowItemNull = """
+      {
+        "appVersion": null,
+        "count": 2
+      }
+    """
+    .filter { !$0.isWhitespace }
+    
+    let resultRowItemNullExample = TopNQueryResultRowItem(metrics: ["count": 2], dimensions: [:], nullValues: ["appVersion"])
+    
+    let resultRowItemOneEntry = """
+      {
+        "appVersion": "335",
+        "count": 2
+      }
+    """
+    .filter { !$0.isWhitespace }
+    
+    let resultRowItemOneItemExample = TopNQueryResultRowItem(metrics: ["count": 2], dimensions: ["appVersion": "335"])
+    
+    let resultRowItemManyEntries = """
+      {
+          "average": 1.25,
+          "count": 88,
+          "dim1": "another_dim1_val",
+          "some_metrics": 28344
+       }
+    """
+    .filter { !$0.isWhitespace }
+    
+    let resultRowItemManyEntriesExample = TopNQueryResultRowItem(metrics: ["count": 88, "some_metrics": 28344, "average": 1.25], dimensions: ["dim1": "another_dim1_val"])
+    
+    func testDecodingEmptyResult() throws {
+        let decodedRows = try JSONDecoder.druidDecoder.decode([TopNQueryResultRow].self, from: emptyResult.data(using: .utf8)!)
+        XCTAssertEqual(decodedRows, decodedEmptyExampleResult)
+    }
+    
+    func testEncodingEmptyResult() throws {
+        let encoded = try JSONEncoder.druidEncoder.encode(decodedEmptyExampleResult)
+        XCTAssertEqual(String(data: encoded, encoding: .utf8)!, emptyResult)
+    }
+    
+    func testDecoding() throws {
+        let decodedRows = try JSONDecoder.druidDecoder.decode([TopNQueryResultRow].self, from: exampleResult.data(using: .utf8)!)
+        XCTAssertEqual(decodedRows, decodedExampleResult)
+    }
+    
+    func testEncoding() throws {
+        let encoded = try JSONEncoder.druidEncoder.encode(decodedExampleResult)
+        XCTAssertEqual(String(data: encoded, encoding: .utf8)!, exampleResult)
+    }
+    
+    func testDecodingTopNQueryResultRowItemNullEntry() throws {
+        let decoded = try JSONDecoder.druidDecoder.decode(TopNQueryResultRowItem.self, from: resultRowItemNull.data(using: .utf8)!)
+        XCTAssertEqual(decoded, resultRowItemNullExample)
+    }
+
+    func testDecodingTopNQueryResultRowItemOneEntry() throws {
+        let decoded = try JSONDecoder.druidDecoder.decode(TopNQueryResultRowItem.self, from: resultRowItemOneEntry.data(using: .utf8)!)
+        XCTAssertEqual(decoded, resultRowItemOneItemExample)
+    }
+    
+    func testDecodingTopNQueryResultRowItemManyEntries() throws {
+        let decoded = try JSONDecoder.druidDecoder.decode(TopNQueryResultRowItem.self, from: resultRowItemManyEntries.data(using: .utf8)!)
+        XCTAssertEqual(decoded, resultRowItemManyEntriesExample)
+    }
+    
+    func testEncodingTopNQueryResultRowItemNullEntry() throws {
+        let encoded = try JSONEncoder.druidEncoder.encode(resultRowItemNullExample)
+        XCTAssertEqual(String(data: encoded, encoding: .utf8)!, resultRowItemNull)
+    }
+
+    func testEncodingTopNQueryResultRowItemOneEntry() throws {
+        let encoded = try JSONEncoder.druidEncoder.encode(resultRowItemOneItemExample)
+        XCTAssertEqual(String(data: encoded, encoding: .utf8)!, resultRowItemOneEntry)
+    }
+    
+    func testEncodingTopNQueryResultRowItemManyEntries() throws {
+        let encoded = try JSONEncoder.druidEncoder.encode(resultRowItemManyEntriesExample)
+        XCTAssertEqual(String(data: encoded, encoding: .utf8)!, resultRowItemManyEntries)
+    }
+}
