cf06356bdbc96a650b74caadb3d50774f72b0ab6
Daniel Jilg <winsmith@winsmith.de>
2022-01-29 17:34:27 +0100

    Add source parameter to registrationrequestbody

--- Sources/DataTransferObjects/DTOs/RegistrationRequestBody.swift
+++ Sources/DataTransferObjects/DTOs/RegistrationRequestBody.swift
@@ -20,6 +20,7 @@ public extension DTOv1 {
         public var receiveMarketingEmails: Bool = false
         public var countryCode: String? = ""
         public var referralCode: String? = nil
+        public var source: String? = nil
 
         public var isValid: ValidationState {
             if organisationName.isEmpty || userFirstName.isEmpty || userEmail.isEmpty || userPassword.isEmpty {
