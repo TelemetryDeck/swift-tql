28c691282bcb184e9f738c35efa3c575610fd082
Daniel Jilg <daniel@telemetrydeck.com>
2025-02-04 12:10:36 +0100

    Update Swift Crypto (#48)

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/TelemetryDeck/SwiftDateOperations.git", from: "1.0.4"),
-        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.8.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
