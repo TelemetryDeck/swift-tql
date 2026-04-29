6ee120ad0bf4bdbd374ba7b78f67b7c9d92b81c4
Daniel Jilg <daniel@telemetrydeck.com>
2026-04-29 16:42:21 +0200

    Funnels with more than 4 steps are no longer realtime

--- Sources/SwiftTQL/QueryGeneration/CustomQuery+Funnel.swift
+++ Sources/SwiftTQL/QueryGeneration/CustomQuery+Funnel.swift
@@ -3,6 +3,23 @@ extension CustomQuery {
         var query = self
 
         guard let steps = steps else { throw QueryGenerationError.keyMissing(reason: "Missing key 'steps'") }
+        
+        // Funnel queries with more than 5 steps regularly break our infrastruture at the moment, but only when they touch
+        // "realtime" data. When they only run on finished segments, they work fine, because historicals have bigger merge
+        // buffers. To mitigate this, we're excluding the last two hours of data from funnels with more than 5 steps.
+        if steps.count > 4, let relativeIntervals = relativeIntervals {
+            var newRelativeIntervals: [RelativeTimeInterval] = []
+            for interval in relativeIntervals {
+                let absoluteInterval = QueryTimeInterval.from(relativeTimeInterval: interval, timeZone: context?.timezone)
+                if absoluteInterval.endDate.timeIntervalSinceNow > -24 * 60 * 60 { // 24 hours ago
+                    newRelativeIntervals.append(.init(beginningDate: interval.beginningDate, endDate: .init(.end, of: .day, adding: -1)))
+                } else {
+                    newRelativeIntervals.append(interval)
+                }
+            }
+            
+            query.relativeIntervals = newRelativeIntervals
+        }
 
         // Generate Filter Statement
         let stepsFilters = Filter.or(.init(fields: steps.compactMap(\.filter)))
